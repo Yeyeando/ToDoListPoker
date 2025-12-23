227e8deb0fc6ab275ed4f4f5cb5afa32642e7d39
Yeyeando <145705687+Yeyeando@users.noreply.github.com>
2025-12-23 23:35:50 +0000

    detalles añadidos

--- .bat
+++ .bat
@@ -1,10 +1,10 @@
 @echo off
 echo Iniciando el frontend...
-cd C:\proyectos\to-do-list\frontend\taskmanager-client
+cd .\frontend\taskmanager-client
 start ng serve
 
 timeout /t 5 /nobreak
 
 echo Iniciando el backend...
-cd C:\proyectos\to-do-list\backend\src\TaskManager.Api
+cd ..\..\backend\src\TaskManager.Api
 start dotnet run
